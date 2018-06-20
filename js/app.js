690580197248e7465695c5889630f566fd681275
Valerie Seer <seervalerie@gmail.com>
2018-06-20 14:29:44 -0500

    Add error and winning functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 const deck = document.getElementsByClassName('deck')[0]
 const movesEl = document.getElementsByClassName('moves')[0]
+const stars = document.getElementsByClassName('stars')[0].children.length
+const restart = document.getElementsByClassName('restart')[0]
+const success = document.getElementsByClassName('success')[0]
+const movesSuccess = document.getElementsByClassName('success__moves')[0]
+const starsSuccess = document.getElementsByClassName('success__stars')[0]
+const resetSuccess = document.getElementsByClassName('success__reset')[0]
 let cards = ['<i class="fa fa-diamond"></i>','<i class="fa fa-paper-plane-o"></i>', '<i class="fa fa-anchor"></i>', '<i class="fa fa-bolt"></i>', '<i class="fa fa-cube"></i>', '<i class="fa fa-leaf"></i>', '<i class="fa fa-anchor"></i>', '<i class="fa fa-bicycle"></i>', '<i class="fa fa-diamond"></i>', '<i class="fa fa-bomb"></i>', '<i class="fa fa-leaf"></i>', '<i class="fa fa-bomb"></i>', '<i class="fa fa-bolt"></i>', '<i class="fa fa-bicycle"></i>', '<i class="fa fa-paper-plane-o"></i>', '<i class="fa fa-cube"></i>']
 let openCards = []
 let matchedCards = []
 let clicks = 0
 let moves = 0
-
+console.log(stars)
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -20,6 +26,19 @@ function shuffle(array) {
     }
 
     return array
+}
+
+function appendCards() {
+    for (card of cards) {
+        let cardEl = document.createElement('LI')
+        cardEl.classList.add('card')
+        cardEl.innerHTML = card
+    
+        deck.appendChild(cardEl)
+    
+        cardEl.addEventListener('click', showCard)
+    }
+    
 }
 
 function incrementMoves() {
@@ -39,6 +58,34 @@ function lockCard(el) {
 function resetCard(el) {
     el.classList.remove('show')
     el.classList.remove('open')
+    el.classList.add('error')
+    setTimeout(function() {
+        el.classList.remove('error')
+    }, 1000)
+}
+
+function restartGame() {
+    matchedCards.length = 0
+    clicks = 0
+    moves = 0
+    movesEl.innerHTML = moves  
+
+    deck.innerHTML = ''
+    shuffle(cards)
+    appendCards()
+}
+
+function showSuccess() {
+    movesSuccess.innerHTML = moves
+    starsSuccess.innerHTML = stars
+    success.classList.remove('is-hidden')
+    success.classList.add('is-flex-visible')
+
+    resetSuccess.addEventListener('click', function() {
+        restartGame()
+        success.classList.remove('is-flex-visible')
+        success.classList.add('is-hidden')
+    })
 }
 
 function showCard() {
@@ -59,29 +106,15 @@ function showCard() {
 
     incrementMoves()
 
+    if (matchedCards.length === 16) {
+        setTimeout(function() {
+            showSuccess()
+        }, 1000)
+    }
 }
 
 movesEl.innerHTML = moves
+restart.addEventListener('click', restartGame)
+
 shuffle(cards)
-
-for (card of cards) {
-    let cardEl = document.createElement('LI')
-    cardEl.classList.add('card')
-    cardEl.innerHTML = card
-
-    deck.appendChild(cardEl)
-
-    cardEl.addEventListener('click', showCard)
-}
-
-
-/*
- * Set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+appendCards()
